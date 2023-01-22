a15cdf8d09bc2b986ad72a037c86cee4964897be
Anna <a_sh33@mail.ru>
2023-01-22 19:35:45 +0200

    Add lesson 2.vinal

--- python_home_2/add.exa.2.3.py
+++ python_home_2/add.exa.2.3.py
@@ -1,20 +1,16 @@
 """Дополнительные задания № 3"""
+
+
 # Реализуйте алгоритм перемешивания списка.
 
 import random
 
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(arr, '\n')
-for el in range(len(arr)):
-    index_al = random.randint(0, len(arr) - 1)
-    temp = arr[el]
-    arr[el] = arr[index_al]
-    arr[index_al] = temp
+for i in range(len(arr)):
+    index_i = random.randint(0, len(arr) - 1)
+    temp = arr[i]
+    arr[i] = arr[index_i]
+    arr[index_i] = temp
 print(arr)
 
-# ИЛИ:
-
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-for el in a:
-    random.shuffle(a)
-print(a)
